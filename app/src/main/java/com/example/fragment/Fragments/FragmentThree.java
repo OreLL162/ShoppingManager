83e1648464b93df996260a01ec041bb33e7ef576
orel <oorreelll199@gmail.com>
2024-02-11 20:46:42 +0200

    FirstCommit

--- app/src/main/java/com/example/fragment/Fragments/FragmentThree.java
+++ app/src/main/java/com/example/fragment/Fragments/FragmentThree.java
@@ -1,13 +1,14 @@
 package com.example.fragment.Fragments;
 
+
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.fragment.R;
 
 /**
@@ -55,6 +56,8 @@ public class FragmentThree extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
